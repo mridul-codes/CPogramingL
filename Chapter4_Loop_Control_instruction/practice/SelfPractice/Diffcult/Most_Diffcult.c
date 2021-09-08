d08ce8226bb6edde0bab5014478c4dc180cbd1ff
Mridul Haloi <trashmridul@gmail.com>
2021-09-09 00:23:40 +0530

    added new file in C4 sum of 10 natural no.

---
+++ Chapter4_Loop_Control_instruction/practice/SelfPractice/Diffcult/Most_Diffcult.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int input, parameter1, parameter2, constant;
+    printf("Enter a no....\t");
+    scanf("%d", &input);
+    parameter1 = input;
+    constant = input;
+    parameter2 = 10 + input;
+
+    while (constant <= parameter2)
+    {
+
+        ++input;
+        constant++;
+        // printf("%d\n", input);
+        parameter1 = parameter1 + input;
+        //    printf("%d\n", constant);
+        if (constant < parameter2)
+        {
+            continue;
+        }
+        else if (constant >= parameter2)
+        {
+            break;
+        }
+    }
+
+    printf("The value of Input %d \n", parameter1);
+
+    return 0;
+}
